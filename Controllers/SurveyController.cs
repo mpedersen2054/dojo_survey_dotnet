549fa1b0c6a76e6f4c318a726498ddf640026802
Matt Pedersen <mattpp@Matts-MacBook-Air.local>
2017-02-13 19:08:10 -0800

    finished

--- Controllers/SurveyController.cs
+++ Controllers/SurveyController.cs
@@ -27,28 +27,20 @@ namespace DojoSurvey.Controllers
         public IActionResult PostResults(string fullName, string location, string language)
         {
             // validation
-            // if (fullName.Length < 1 || location.Length < 1 || language.Length < 1)
-            // {
-            //     return RedirectToAction("Index");
-            // }
-            // else
-            // {
-            //     ViewBag.fullName = fullName;
-            //     ViewBag.location = location;
-            //     ViewBag.language = language;
-            //     System.Console.WriteLine(fullName);
-            //     System.Console.WriteLine(location);
-            //     System.Console.WriteLine(language);
-            //     return View("Results");
-            // }
-
-            ViewBag.fullName = fullName;
-            ViewBag.location = location;
-            ViewBag.language = language;
-            System.Console.WriteLine(fullName);
-            System.Console.WriteLine(location);
-            System.Console.WriteLine(language);
-            return View("Results");
+            if (fullName.Length < 1 || location.Length < 1 || language.Length < 1)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.fullName = fullName;
+                ViewBag.location = location;
+                ViewBag.language = language;
+                System.Console.WriteLine(fullName);
+                System.Console.WriteLine(location);
+                System.Console.WriteLine(language);
+                return View("Results");
+            }
         }
     }
 }

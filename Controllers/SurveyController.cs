89d8084f57ba6c4925e07479129a2d7857ca7992
Matt Pedersen <mattpp@Matts-MacBook-Air.local>
2017-02-13 18:09:17 -0800

    write code for results page

--- Controllers/SurveyController.cs
+++ Controllers/SurveyController.cs
@@ -13,11 +13,42 @@ namespace DojoSurvey.Controllers
             return View("Index");
         }
 
+        // default route that will redirect back
+        // to index if there is no form data
         [HttpGet]
         [Route("results")]
         public IActionResult Results()
         {
             return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [Route("results")]
+        public IActionResult PostResults(string fullName, string location, string language)
+        {
+            // validation
+            // if (fullName.Length < 1 || location.Length < 1 || language.Length < 1)
+            // {
+            //     return RedirectToAction("Index");
+            // }
+            // else
+            // {
+            //     ViewBag.fullName = fullName;
+            //     ViewBag.location = location;
+            //     ViewBag.language = language;
+            //     System.Console.WriteLine(fullName);
+            //     System.Console.WriteLine(location);
+            //     System.Console.WriteLine(language);
+            //     return View("Results");
+            // }
+
+            ViewBag.fullName = fullName;
+            ViewBag.location = location;
+            ViewBag.language = language;
+            System.Console.WriteLine(fullName);
+            System.Console.WriteLine(location);
+            System.Console.WriteLine(language);
+            return View("Results");
         }
     }
 }
